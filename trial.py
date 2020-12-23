797b3dbb23b904e8014b0cbf57b26ae1441df7d8
mohammed081296 <59499530+mohammed081296@users.noreply.github.com>
2020-12-23 15:17:53 -0600

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,11 +1,38 @@
-class Vehicle:
-    def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed=max_speed
-        self.mileage=mileage
-class bus(Vehicle):
-    def __str__(self):
-        return ("vehicle name {}, Speed {}, Mileage {}".format(self.name, self.max_speed, self.mileage))
-
-School_bus = bus("school volvo", 180, 12)
-print(School_bus)
+people = int(input("how many people are there: "))
+i = 0
+while i < people:
+    i+=1
+    person = str(input("Are you a student?: "))
+    x = str(input("Name please: "))
+    y = str(input("Address please: "))
+    z = str(input("City please: "))
+    
+    
+    
+    class trial:
+        amount_of_people = 0
+        def __init__(self, name, address, city):
+            self.name = name
+            self.address = address
+            self.city = city
+            trial.amount_of_people +=1
+        def __str__(self):
+            return ("Name {}, Address {}, City {}".format(self.name, self.address, self.city))
+    
+    class student(trial):
+        def __init__(self, grade):
+            self.grade = grade
+        def __str__(self):
+            return "Grade {}".format(self.grade)
+        
+        
+    if person == "yes":
+        g = str(input("what grade are you in: "))
+        student(g)
+        print(trial(x,y,z))
+        print(student(g))
+            
+    else:    
+        person1 = trial(x,y,z,)
+        print(person1)
+    print(trial.amount_of_people)
